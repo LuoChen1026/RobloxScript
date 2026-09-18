0fd833e51fdeafbc88c858295230344d2906990c
星火 <3090690365@qq.com>
2026-09-19 05:44:15 +0800

    Update OE-Xinghuo(key).lua

--- OE-Xinghuo(key).lua
+++ OE-Xinghuo(key).lua
@@ -66,7 +66,7 @@ end
 Arqel.Callbacks.OnSuccess = function()
     Arqel:Notify("成功", "验证通过!", 2, "success")
     if getgenv().USER_TIER == "premium" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/1jjtjdjjgj66653/nc2tx/refs/heads/main/oe.lua"))()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Our-Execution-OE-137018"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/LuoChen1026/RobloxScript/main/%E6%88%91%E4%BB%AC%E7%9A%84%E5%A4%84%E5%86%B3%E5%9F%BA%E7%A1%80%E7%89%88.lua"))()
     end
